2f88c9537325f4fba6d8f61379c5433b576c583d
Adrien.B <baffioniadrien23@gmail.com>
2025-01-17 10:35:06 +0100

    remove nav

--- src/components/header.py
+++ src/components/header.py
@@ -4,7 +4,5 @@ from src.components.navbar import create_navbar
 
 def create_header():
     return html.Header([
-        # html.H1("Tableau de Bord d'Analyse Météorologique", className='text-center'),
         html.P("Tableau de Bord Météo", className="header-title"),
-        create_navbar(),
     ])
